7d96fa98fd6241536f86b948924614dbf9b919f5
ToXicItself <itselftoxic@gmail.com>
2023-10-02 00:01:50 +0530

    Some more Ques

---
+++ Day 2/Patterns.cpp
@@ -0,0 +1,60 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=0;
+//     while (i!=5)
+//     {
+//         cout<<"*****"<<endl;
+//         i++;
+//     }
+    
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string j="";
+//     for (int i = 0; i < 5; i++)
+//     {
+//         string f= j+"*";
+//         cout<<f<<endl;
+//          j=f;
+//     }
+    
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string j="";
+//     for (int i = 1; i < 6; i++)
+//     {
+        
+//         string l= to_string(i);
+//         string f= j+l;
+//         cout<<f<<endl;
+//          j=f;
+//      }
+// }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        int j = 0;
+        while (i>j)
+        {
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        
+    }
+}
+
+
+
