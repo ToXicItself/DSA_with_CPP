e7167cfceeb860df9ef0d96d4a79c8831b88d9fd
ToXicItself <itselftoxic@gmail.com>
2023-10-07 20:26:57 +0530

    Patterns

--- Day 2/Patterns.cpp
+++ Day 2/Patterns.cpp
@@ -79,7 +79,7 @@ void pattern7()
 {
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 4; j >i; j--)
+        for (int j = 4; j > i; j--)
         {
             cout << " ";
         }
@@ -87,40 +87,126 @@ void pattern7()
         {
             cout << "*";
         }
-        for (int j = 4; j >i; j--)
+        for (int j = 4; j > i; j--)
         {
             cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void pattern8(){
+void pattern8()
+{
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j<i; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
         }
-        for (int j = 0; j<9-(2*i); j++)
+        for (int j = 0; j < 9 - (2 * i); j++)
         {
             cout << "*";
         }
-        for (int j = 0; j<i; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern9()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 4; j > i; j--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 4; j > i; j--)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 9 - (2 * i); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern10()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;}
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 4; j > i; j--)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+
+}
+
+void pattern11(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+             int n=(i+j+1)%2;
+            cout<<n;
         }
         cout<<endl;
     }
 
 }
+
+void pattern12(){
+    for(int i=1;i<5;i++){
+        for(int j=1;j<i+1;j++){
+            cout<<j;
+        }
+        for(int j=8;j>(i*2);j--){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
     // pattern5(n);
     // pattern6(n);
     // pattern7();
-    pattern8();
-
+    // pattern8();
+    // pattern9();
+    //pattern10();
+    // pattern11();
+     pattern12();
+    return 0;
 }
--- Day 2/tempCodeRunnerFile.cpp
+++ Day 2/tempCodeRunnerFile.cpp
@@ -1,2 +1,23 @@
 
+            cout<<" ";
+        }
+        for(int j=i;j>=(i+1);j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    // int n;
+    // cin >> n;
+    // pattern5(n);
+    // pattern6(n);
+    // pattern7();
+    // pattern8();
+    // pattern9();
+    //pattern10();
+    // pattern11();
+     pattern12();
+    return 0;
 }
